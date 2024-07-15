47c4b245ffd9f037a92631a0d0fc19f2fe688fe2
Steven Wanderski <steven.wanderski@gmail.com>
2024-07-14 20:48:32 -0500

    Adds production default url options

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,5 @@ Rails.application.configure do
   # ]
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+  config.action_mailer.default_url_options = { host: 'dolistapp.org' }
 end
